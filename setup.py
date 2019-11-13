faf7e3470ab227c7c2a0e2b12a2119c4b0793ffa
pmla <pete.mahler.larsen@gmail.com>
2019-11-13 16:19:54 +0100

    read long_description from README.md

--- setup.py
+++ setup.py
@@ -1,46 +1,51 @@
 import os
 import sys
 import numpy
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 major_version = 0
 minor_version = 1
-subminor_version = 1
+subminor_version = 2
 
 python_version = "%d.%d%s" % (sys.version_info.major,
                               sys.version_info.minor,
                               sys.abiflags)
 
-module1 = Extension('auguste',
-                    define_macros = [('MAJOR_VERSION', str(major_version)),
-                                     ('MINOR_VERSION', str(minor_version))],
-                    include_dirs = [os.path.join(numpy.get_include(), 'numpy')],
-                    libraries = [],
-                    library_dirs = [],
-                    sources = ['eigendecomposition.cpp',
-                               'lup_decomposition.cpp',
-                               'mahalonobis_transform.cpp',
-                               'matrix_vector.cpp',
-                               'minkowski_reduction.cpp',
-                               'parse_string.cpp',
-                               'polar_decomposition.cpp',
-                               'quaternion.cpp',
-                               'sqp_newton_lagrange.cpp',
-                               'stepwise_iteration.cpp',
-                               'symmetrization.cpp',
-                               'unimodular_functions.cpp',
-                               'auguste_module.cpp',
+
+# read the contents of README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+module = Extension('auguste',
+                   define_macros = [('MAJOR_VERSION', str(major_version)),
+                                    ('MINOR_VERSION', str(minor_version))],
+                   include_dirs = [os.path.join(numpy.get_include(), 'numpy')],
+                   libraries = [],
+                   library_dirs = [],
+                   sources = ['eigendecomposition.cpp',
+                              'lup_decomposition.cpp',
+                              'mahalonobis_transform.cpp',
+                              'matrix_vector.cpp',
+                              'minkowski_reduction.cpp',
+                              'parse_string.cpp',
+                              'polar_decomposition.cpp',
+                              'quaternion.cpp',
+                              'sqp_newton_lagrange.cpp',
+                              'stepwise_iteration.cpp',
+                              'symmetrization.cpp',
+                              'unimodular_functions.cpp',
+                              'auguste_module.cpp',
 ])
 
 setup(name = 'auguste',
-       version = '%d.%d.%d' % (major_version, minor_version, subminor_version),
-       description = 'Minimum-strain Bravais lattice symmetrization',
-       author = 'P. M. Larsen',
-       author_email = 'pmla@fysik.dtu.dk',
-       url = 'https://github.com/pmla/auguste',
-       long_description = '''
-A python module for minimum-strain symmetrization of Bravais lattices.
-The module can compute a distance from each Bravais lattice type, and
-symmetrize an arbitrary lattice to a target Bravais type.''',
-       ext_modules = [module1]
+      ext_modules = [module],
+      version = '%d.%d.%d' % (major_version, minor_version, subminor_version),
+      description = 'Minimum-strain symmetrization of Bravais lattices',
+      author = 'P. M. Larsen',
+      author_email = 'pmla@fysik.dtu.dk',
+      url = 'https://github.com/pmla/auguste',
+      long_description_content_type='text/markdown',
+      long_description=long_description,
 )
