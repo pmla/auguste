5815a9bcdb8b5641c4539258777e493814a32e0a
pmla <peter.mahler.larsen@gmail.com>
2020-05-14 19:10:40 +0200

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import platform
 
 major_version = 0
 minor_version = 1
-subminor_version = 4
+subminor_version = 5
 
 python_version = "%d.%d%s" % (sys.version_info.major,
                               sys.version_info.minor,
